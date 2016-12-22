b600e29ab0d61cbebed3eb86778b580668901149
Sourabh Kapoor <kapoor.utd@gmail.com>
2016-12-22 12:39:51 +0530

    Crash Fix Related To Keyboard

--- app/src/main/java/org/wizbots/labtab/util/LabTabUtil.java
+++ app/src/main/java/org/wizbots/labtab/util/LabTabUtil.java
@@ -68,7 +68,9 @@ public class LabTabUtil implements LabTabConstants {
 
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        if (activity.getCurrentFocus() != null) {
+            inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        }
     }
 
     public static String getRandomLabLevel() {
