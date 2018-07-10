b7b7ea0601cf695ae3109db2e3e130b7785f98ca
jsharma <jsharma@craterzone.com>
2018-07-10 11:13:22 +0530

    Crash report added

--- app/src/main/java/org/wizbots/labtab/activity/ResultActivity.java
+++ app/src/main/java/org/wizbots/labtab/activity/ResultActivity.java
@@ -2,18 +2,51 @@ package org.wizbots.labtab.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
 
 import org.wizbots.labtab.R;
 
 public class ResultActivity extends AppCompatActivity {
 
     private TextView textView ;
+    private Button crashButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         textView = (TextView)findViewById(R.id.textView1);
         textView.setText("Welcome to the Result Activity");
+
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Crashlytics.getInstance().crash(); // Force a crash
+            }
+        });
+        addContentView(crashButton,
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+//        crashButton =(Button) findViewById(R.id.crash_button);
+//        crashButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Crashlytics.getInstance().crash(); // Force a crash
+//            }
+//        });
+
+//        addContentView(crashButton,
+//                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+//                        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        
     }
+
+
 }
